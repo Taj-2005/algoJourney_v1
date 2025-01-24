8c3accb2a1bf3a7b6eb44120b932ef0ae075f19f
Taj-786 <tajuddinshaik786r@gmail.com>
2025-01-25 04:15:09 +0530

    dsa

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
     }
   return (
     <header className="alignment">
-            <h1 className='logo'>AlgoJourney</h1>
+            <h1 className='h1'>AlgoJourney</h1>
             <div className='user-icon-wrapper' onMouseEnter={handleMouseEnter}>
                 <img className='icon' src={userProfile} alt='User Icon' />
                 {showTooltip && (
